ff8450bd5c2c3a983c61134d6f7e33f9a88030d7
Vincent Woltmann <vincent@woltmann.art>
2023-06-05 22:13:57 +0200

    Moved VS Code config to nix

--- hosts/athens/titania/home-manager.nix
+++ hosts/athens/titania/home-manager.nix
@@ -38,7 +38,27 @@
     vscode = {
       enable = true;
       package = pkgs.vscodium;
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
       mutableExtensionsDir = false;
+      userSettings = {
+        # Just open untrustedFiles in a trusted Workspace
+        security.workspace.trust.untrustedFiles = "open";
+        # Restart on env change, to directly reflect new dependencies
+        direnv.restart.automatic = true;
+        # TODO: Fill concrete path of direnv executable
+        direnv.path.executable = pkgs.direnv + /bin/direnv;
+        jupyter.kernels.filter = [
+            {
+              path = "/run/current-system/sw/bin/python";
+              type = "pythonEnvironment";
+            }
+            {
+              path = "/usr/bin/python3";
+              type = "pythonEnvironment";
+            }
+        ];
+      };
       extensions = with pkgs.vscode-extensions; [
         vscodevim.vim
         rust-lang.rust-analyzer
