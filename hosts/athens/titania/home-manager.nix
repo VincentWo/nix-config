3cf94b3d284df248eafd2bc7547c1bb041ffc85c
Vincent Woltmann <vincent@woltmann.art>
2023-06-08 12:10:11 +0200

    Added .CFUserTextEncoding

--- hosts/athens/titania/home-manager.nix
+++ hosts/athens/titania/home-manager.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
   home = {
     stateVersion = "23.11";
+    file = {
+      # Controls the preferred encoding + locale for CoreFoundation
+      # 0x08000100 = UTF-8
+      # 0 = English
+      ".CFUserTextEncoding".text = "0x08000100:0";
+    };
   };
 
   programs = {
